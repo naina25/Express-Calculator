074fe413ee9f637c2d4f970a746e58e59835fabc
naina <nanu02d8@gmail.com>
2020-10-20 00:57:25 +0530

    Added a POST method to calculate the sum

--- calculator.js
+++ calculator.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/",(req,res) =>{
     res.sendFile(`${__dirname}/index.html`);
+})
+app.post("/",(req,res) =>{
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let sum = num1 + num2 ;
+    res.send(`The sum of given two numbers is ${sum}`);
 })
 app.listen(3000, () =>{
     console.log("server has started on port 3000");
